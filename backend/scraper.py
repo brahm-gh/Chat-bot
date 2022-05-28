f9834d77930dd10950d09dd15d6f99baed171bfe
Brahim Ghaouthi <abraham.ght@hotmail.com>
2022-05-28 09:33:36 +0100

    matching keyword to url

--- backend/scraper.py
+++ backend/scraper.py
@@ -1,12 +1,40 @@
 from bs4 import BeautifulSoup
 import requests
+import json
 
 data = requests.get('https://www.allrecipes.com/recipes/').text
 soup = BeautifulSoup(data, 'html5lib')
 
 keyword = ' '
+keyword = 'drinks'
 
 l = []
 for target in soup.find_all('script'):
-        l.append(target)
-print(l[-1])
+        l.append(target.getText())
+obj = l[-1]
+obj = json.loads(obj)
+obj = obj[-1]
+dict = {}
+dict['name'] = obj['name']
+dict['url'] = obj['url']
+url = ''
+found = False
+for i in range(len(dict['name'][0])):
+
+        if dict['name'][0][i].lower() == keyword:
+            url = dict['url'][0][i]
+            found = True
+if not found:
+    print('No match')
+
+
+
+
+
+
+#debugging
+#print(dict)
+#print (dict['name'][0][1])
+#print(dict['url'][0][1])
+print(url)
+
