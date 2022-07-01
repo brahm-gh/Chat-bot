0b6538a7a2dbb9746f83993a77182050c3014d75
Brahim Ghaouthi <73402390+brahm-gh@users.noreply.github.com>
2022-07-01 06:15:05 +0100

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import * as ReactDOM from 'react-dom/client';
-import Chatbot from './Chatbot';
-import "./index.css"
-
-const container = document.getElementById('chatbot');
-const cb = ReactDOM.createRoot(container);
-cb.render(<Chatbot />)
