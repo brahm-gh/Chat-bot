ddb85a0bf78ee6187a4c2370cbbc038dfa1dc99f
Brahim Ghaouthi <brahim.ghaouthi@stud.th-deg.de>
2022-07-01 07:07:23 +0200

    add scraped data in text files

--- backend/scraper.py
+++ backend/scraper.py
@@ -1,13 +1,17 @@
+#input keyword, output answer
 from bs4 import BeautifulSoup
 import requests
 import json
-import pprint
+import re
+
 
 data = requests.get('https://www.allrecipes.com/recipes/').text
 soup = BeautifulSoup(data, 'html5lib')
 
 keyword = ' '
-keyword = 'drinks'
+keyword = 'drinks' #will be given by user via socket
+with open('keywords.txt', 'r') as f:
+    keyword = f.readline()
 
 l = []
 for target in soup.find_all('script'):
@@ -20,24 +24,34 @@ dict['name'] = obj['name']
 dict['url'] = obj['url']
 url = ''
 found = False
+items = []
+sites = []
 for i in range(len(dict['name'][0])):
 
+        items.append(dict['name'][0][i])
+        sites.append(dict['url'][0][i])
         if dict['name'][0][i].lower() == keyword:
             url = dict['url'][0][i]
             found = True
+
+with open('items.txt', 'a') as f:
+    for item in items:
+        f.write(item)
+        f.write('\n')
+
+with open('sites.txt', 'a') as f:
+    for site in sites:
+        f.write(site)
+        f.write('\n')
+
+print(items)
+print(sites)
 if not found:
     print('No match')
 
 
-
-
-
-
-#debugging
-#print(dict)
-#print (dict['name'][0][1])
-#print(dict['url'][0][1])
-print(url)
+def display_items():
+    return items
 
 
 data2 = requests.get(url).text
@@ -53,34 +67,104 @@ obj2= json.loads(obj2)
 obj2 = obj2[1]
 obj2 = obj2['itemListElement']
 
-keyword2 = ''
-#match url to keyword2 (regex)
 
-for i in range(len(obj2)):
-    print(obj2[i]['url'])
+sublist = []
+for j in range(len(sites)):
+    datax = requests.get(sites[j]).text
+    soupx = BeautifulSoup(datax, 'html5lib')
 
-url2 = obj2[0]['url'] #trial url (will depend on keyword2)
+    lx = []
+
+    for target in soupx.find_all('script'):
+        lx.append(target.getText())
+    if j!= 7 and j!=12 and j!=14 and j!=19 and j!=21:
+        objx = lx[-1]
+        objx = json.loads(objx)
+        objx = objx[1]
+        objx = objx['itemListElement']
+
+        print(items[j])
+        sublist.append(items[j])
+        try:
+            for k in range(len(objx)):
+                print(objx[k]['url'])
+                sublist.append(objx[k]['url'])
+        except KeyError:
+            print('no url')
+
+with open('sublist.txt', 'a') as f:
+    for item in sublist:
+        f.write(item)
+        f.write('\n')
+
+
+keyword2 = 'old fashioned lemonade' #will be given by the user via the socket
+
+
+keyword2 = re.sub('\s', '-', keyword2)
+
+#url2 = sublist[5] #trial url (will depend on keyword2) will be removed
+
+print(keyword2)
+for i in range(len(sublist)):
+    n, m = len(sublist[i]), len(keyword2)
+    for j in range(n-m+1):
+        k = 0
+        while k < m and sublist[i][j+k] == keyword2[k]:
+            k += 1
+        if k == m:
+            url2 = sublist[i]
+            break
+    if k == m:
+        break
+
+if k != m:
+    print('No match found between url and keyword')
+print(url2)
+
+
+for url in sublist:
+    if len(url) > 42:
+        data3 = requests.get(url).text
+        soup3 = BeautifulSoup(data3, 'html5lib')
+        l3 = []
+        l4 = []
+        for target in soup3.find_all('script'):
+                l3.append(target.getText())
+        for target in soup3.find_all('title'):
+                l4.append(target.getText())
+        title = l4[0].split('|')
+        title = title[0]
+        print(title)
+        obj3 = l3[-1]
+        obj3= json.loads(obj3)
+        obj3 = obj3[1]
+        try:
+            recipe_ingredients = obj3["recipeIngredient"]
+            ingredients = 'Ingredients:\n'
+            for i in range(len(recipe_ingredients)):
+                ingredients += recipe_ingredients[i] + '\n'
+            instructions = ''
+            for i in range(len(obj3["recipeInstructions"])):
+                instructions += obj3["recipeInstructions"][i]['text']
+
+            with open('recipes.txt', 'a') as f:
+                f.write('\n')
+                f.write(title)
+                f.write('\n')
+                f.write(str(ingredients.encode('utf8')))
+                f.write('\n')
+                f.write(instructions)
+                f.write('\n')
+                f.write('\n')
+
+        except KeyError:
+            print('no key found')
+        except:
+            print('no value')
 
 
 
-
-data3 = requests.get(url2).text
-soup3 = BeautifulSoup(data3, 'html5lib')
-
-l3 = []
-for target in soup3.find_all('script'):
-        l3.append(target.getText())
-
-obj3 = l3[-1]
-obj3= json.loads(obj3)
-obj3 = obj3[1]
-recipe_ingredients = obj3["recipeIngredient"]
-ingredients = 'Ingredients:\n'
-for i in range(len(recipe_ingredients)):
-    ingredients += recipe_ingredients[i] + '\n'
-instructions = ''
-for i in range(len(obj3["recipeInstructions"])):
-    instructions += obj3["recipeInstructions"][i]['text']
 
 
 print(ingredients)
--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const http = require('http');
 const server = http.createServer(app);
 
 const { Server } = require('socket.io');
+const fs = require("fs");
 const io = new Server(server);
 
 server.listen(5000, function () {
@@ -21,12 +22,50 @@ io.on("connection", (socket) => {
 
     socket.on("disconnect", (reason) => {
         console.log(`disconnect ${socket.id} due to ${reason}`);
+        //start of conversation
+        opening = "Hello, I am your personal AI assistant, what recipe are you looking for?"
+        socket.emit("opening", opening);
     });
 
+    //user's question
     socket.on("question", (data) => {
         console.log("recieved question: "+data)
         // place your bot-code here !!!
-        answer = "I am dumb, i cannot answer to \"" + data + "\" yet";
-        socket.emit("answer", answer);
+        const fs = require('fs')
+
+        //write the question (keyword) to a text file
+        fs.writeFile('keywords.txt', data, err => {
+          if (err) {
+            console.error(err)
+            return
+          }
+        })
+        //answer of the bot
+        new_answer= "I have found several " + data + " recipes. Here are the results:"
+        socket.emit("new question", new_answer)
+        fs.readFile('sublist.txt', 'utf8', (err, data1) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          data1 =  data1.toString().split("\n");
+          return data1;
+        });
+        //looking for the keyword in the file
+        for (let i = 0; i <  data1.length; i++) {
+            if (data1[i] == data) {
+                list = []
+                j = i + 1
+                while (data1[j].length > 30) {
+                    list += data[j]
+                }
+            }
+        }
+        //socket.emit()
+        socket.emit("list", list)
+        if (list.length < 1) {
+            answer = "I am dumb, I cannot answer to \"" + data + "\" yet, can you rephrase?";
+            socket.emit("answer", answer);
+        }
     });
 });
